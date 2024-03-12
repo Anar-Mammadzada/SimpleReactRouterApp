9ee8049ee467046a7913cdcd148b4d6c8190f8ab
Anar-Mammadzada <anarmammadza@gmail.com>
2024-03-12 17:47:51 +0400

    Install, import React Router and use React Portal for modal dialogues

---
+++ react-router/src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Contact from "./components/Contact";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+    <div className="App">
+      <Navbar />
+      <Routes>
+      <Route exact path="/" Component={Home}></Route>
+      <Route path="/about" Component={About}></Route>
+      <Route path="/contact" Component={Contact}></Route>
+      </Routes>
+    </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ react-router/src/components/Contact.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Modal from "./Modal";
+
+const Contact = (props) => {
+
+    return (
+        <div>
+            <Modal />
+            <div className="ui raised very padded text container segment" style={{ marginTop: '80px' }}>
+                <h3 className="ui header">Contact</h3>
+                <p>Salam ay contact qaqa sen necesen</p>
+            </div>
+        </div>
+
+    )
+}
+
+export default Contact;
