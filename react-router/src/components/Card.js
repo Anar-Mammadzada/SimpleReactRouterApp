923f0887451f58bd4e29017a9da11b42675ac1e6
Anar-Mammadzada <anarmammadza@gmail.com>
2024-03-14 12:32:16 +0400

    Install and add Redux-Thunk

--- react-router/src/components/Card.js
+++ react-router/src/components/Card.js
@@ -1,26 +1,43 @@
 import React from "react";
 import { connect } from "react-redux";
+import { deleteCard, fetchUser } from "../actions/cardActions";
 
 class Card extends React.Component {
 
+    componentDidMount() {
+        this.props.fetchUser();
+    }
 
     render() {
-        const {title, body} = this.props.card;
+        const { users } = this.props;
         return (
-            <div className="ui raised very padded text container segment" style={{ marginTop: '80px' }}>
-                <h3 className="ui header">{title}</h3>
-                <p>{body}</p>
-            </div>
+            users.map(user => {
+                return (
+                    <div key={user.id} className="ui raised very padded text container segment" style={{ marginTop: '80px' }}>
+                        <h3 className="ui header">{user.name}</h3>
+                        <p>{user.email}</p>
+                    </div>
+                )
+            })
         )
 
     }
 }
 
-const mapStateToProps = (state, ownProps) =>{
+const mapStateToProps = (state, ownProps) => {
     let title = ownProps.match.params.user;
-    return{
-        card : state.cards.find(card => card.title === title)
+    return {
+        card: state.cards.find(card => card.title === title),
+        users: state.users
     }
 }
 
-export default connect(mapStateToProps) (Card);
+const mapDispatchToProps = (dispatch) => {
+    return {
+        deleteCard: (id) => { dispatch(deleteCard(id)) },
+        fetchUser: () => { dispatch(fetchUser()) }
+    }
+}
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(Card);
--- react-router/src/reducers/rootReducer.js
+++ react-router/src/reducers/rootReducer.js
@@ -4,11 +4,28 @@ const initialState = {
     {id: '1', title: 'Anar', body: 'Hello Anar, how are you?'},
     {id: '2', title: 'Ugur', body: 'Hello Ugur, how are you?'},
     {id: '3', title: 'Sanan', body: 'Hello Sanan, how are you?'}
-  ]
+  ],
+  users : []
 }
 
 const rootReducer = (state = initialState, action)=>{
-  return state;
+ switch(action.type){
+  case 'DELETE_CARD' :
+    let newCards = state.cards.filter(card=>card.id !== action.id)
+    return{
+      ...state,
+      cards : newCards
+    }
+
+    case 'FETCH_USER' :
+      return{
+        ...state,
+        users : action.payload
+      }
+
+    default :
+    return state;
+ }
 }
 
 export default rootReducer;
